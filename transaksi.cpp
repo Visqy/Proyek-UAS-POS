5cc703e3def252918def3340455f1a0c11418898
asrieeputri <135189883+asrieeputri@users.noreply.github.com>
2023-06-01 12:19:23 +0700

    fix beres kata sabiq

--- transaksi.cpp
+++ transaksi.cpp
@@ -10,7 +10,7 @@
 
 using namespace std;
 
-void tambahTransaksi(vector<Transaksi> &transaksiList, sqlite3 *db)
+void tambahTransaksi(vector<Transaksi>& transaksiList, sqlite3* db)
 {
     char jawaban = 'y';
 
@@ -18,20 +18,19 @@ void tambahTransaksi(vector<Transaksi> &transaksiList, sqlite3 *db)
     {
         Transaksi transaksi;
 
-        // Generate nomor transaksi secara acak
-        srand(time(0));
-        transaksi.nomorTransaksi = rand() % 1000 + 1;
+        cout << "Masukkan nama pengguna: ";
+        cin >> transaksi.namaUser;
 
         // Mendapatkan waktu saat ini
         time_t now = time(0);
-        struct tm *timeInfo = localtime(&now);
+        struct tm* timeInfo = localtime(&now);
 
         stringstream ss;
         ss << put_time(timeInfo, "%Y-%m-%d %H:%M:%S");
         transaksi.waktuTransaksi = ss.str();
 
-        cout << "Masukkan nama pengguna: ";
-        cin >> transaksi.namaUser;
+        cout << "Masukkan nomor transaksi : ";
+        cin >> transaksi.nomorTransaksi;
 
         int harga = 0;
         char tambahBarangJawaban = 'y';
@@ -68,9 +67,39 @@ void tambahTransaksi(vector<Transaksi> &transaksiList, sqlite3 *db)
     }
 }
 
+void printReceipt(const vector<Transaksi>& transaksiList)
+{
+    for (const Transaksi& transaksi : transaksiList)
+    {
+        cout << "======================================" << endl;
+        cout << "            STRUK BELANJA             " << endl;
+        cout << "======================================" << endl;
+        cout << "Nomor Transaksi: " << transaksi.nomorTransaksi << endl;
+        cout << "Waktu Transaksi: " << transaksi.waktuTransaksi << endl;
+        cout << "Nama Pengguna: " << transaksi.namaUser << endl;
+        cout << "Daftar Barang:" << endl;
+        cout << left << setw(5) << "No" << setw(15) << "NAMA BARANG" << setw(10) << "JUMLAH" << setw(15) << "HARGA PER ITEM" << setw(15) << "TOTAL HARGA" << endl;
+
+        double total = 0.0;
+        int count = 1;
+        for (const Barang& barang : transaksi.daftarBarangTransaksi)
+        {
+            double itemTotal = barang.quantitas * barang.hargaPerBarang;
+            total += itemTotal;
+
+            cout << left << setw(5) << count << setw(15) << barang.nama << setw(10) << barang.quantitas << setw(15) << barang.hargaPerBarang << setw(15) << itemTotal << endl;
+            count++;
+        }
+
+        cout << "======================================" << endl;
+        cout << "TOTAL: " << total << endl;
+        cout << "======================================" << endl;
+    }
+}
+
 int main()
 {
-    sqlite3 *db;
+    sqlite3* db;
     if (!checkDB(db))
     {
         cout << "Gagal memeriksa database" << endl;
@@ -78,7 +107,9 @@ int main()
     }
 
     tambahTransaksi(TransaksiList, db);
+    printReceipt(TransaksiList);
 
     sqlite3_close(db);
+
     return 0;
 }
