ac2db41018c0569345938e19a7105664e46886f4
Visqy <62652907+Visqy@users.noreply.github.com>
2023-06-01 13:03:20 +0700

    test commit 1

--- laporan.cpp
+++ laporan.cpp
@@ -1,21 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <string>
+#include <filesystem>
+#include <sqlite3.h>
+#include "database.cpp"
 
 using namespace std;
 
-struct Barang
+bool tampilTransaksi(sqlite3 *db)
 {
-    string nama;
-    int quantitas;
-    int hargaPerBarang;
-};
+    if (!readTransaksi(db))
+    {
+        cout << "Gagal membaca tabel transaksi" << endl;
+        return false;
+    }
+    else
+    {
+        cout << "-----------------------------------------------------------------------------------------" << endl;
+        cout << "Kode Transaksi        Waktu Transaksi            Harga Total            User            " << endl;
+        cout << "-----------------------------------------------------------------------------------------" << endl;
+        for (const auto &transaksi : TransaksiList)
+        {
+            cout << setiosflags(ios::left) << setw(15) << transaksi.nomorTransaksi;
+            cout << setiosflags(ios::left) << setw(23) << transaksi.waktuTransaksi;
+            cout << setiosflags(ios::left) << setw(20) << transaksi.hargaTransaksi;
+            cout << setiosflags(ios::left) << setw(20) << transaksi.namaUser;
+            cout << endl;
+        }
+        cout << "---------------------------------------------------------------" << endl;
+        return true;
+    }
+}
 
-struct Transaksi
+int main()
 {
-    int nomorTransaksi;
-    string waktuTransaksi;
-    vector<Barang> daftarBarangTransaksi;
-    string namaUser;
-};
-
-vector<Transaksi> Transaksi;
+    sqlite3 *db;
+    if (!checkDB(db))
+    {
+        cout << "Gagal memeriksa database" << endl;
+        return 1;
+    }
+    tampilTransaksi(db);
+    sqlite3_close(db);
+    return 0;
+}
