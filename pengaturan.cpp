dc6547dd85763f919d17691fd7ff856964c088c9
vaioretto22 <134789893+vaioretto22@users.noreply.github.com>
2023-06-03 22:15:33 +0700

    Add files via upload
    Bismillah semoga tidak ada yang eror ya allah

--- pengaturan.cpp
+++ pengaturan.cpp
@@ -3,22 +3,9 @@
 #include <string>
 #include <filesystem>
 #include <sqlite3.h>
-#include "database.cpp"
 
 using namespace std;
 // buat nambah user baru
-void FixCin()
-{
-    if (cin.fail())
-    {
-        cin.clear();
-        cin.ignore(256, '\n');
-    }
-    else
-    {
-        cin.ignore();
-    }
-};
 void tambahUser(sqlite3 *db)
 {
     bool go = false;
@@ -32,16 +19,19 @@ void tambahUser(sqlite3 *db)
     cin >> newUser.pass;
     do
     {
-    cout << "Masukkan type: ";
-    cin >> newUser.type;
-    if(newUser.type >3 || newUser.type <1) {
-        cout << "Type tidak tersedia" << endl;
-        go = true;
-    } else {
-        go = false;
-    }
+        cout << "Masukkan type: ";
+        cin >> newUser.type;
+        if (newUser.type > 3 || newUser.type < 1)
+        {
+            cout << "Type tidak tersedia" << endl;
+            go = true;
+        }
+        else
+        {
+            go = false;
+        }
 
-    }while(go);
+    } while (go);
 
     vector<string> userValues = {newUser.id, newUser.pass, to_string(newUser.type)};
     if (insertDB(db, "USER", userColumns, userValues))
@@ -125,95 +115,161 @@ void updatePassUser(sqlite3 *db)
     system("pause");
     system("cls");
 }
-void manageUser(sqlite3 *db);
-void menuPengaturan(sqlite3 *db);
 
-void manageUser(sqlite3 *db)
+void manageUser(sqlite3 *db, int &type, string &userSekarang)
 {
     int pil;
-    do{
-    cout << "Menu Management User" << endl;
-    cout << "1.     Tambah User            " << endl;
-    cout << "2.     Delete User            " << endl;
-    cout << "3.     Update Password User   " << endl;
-    cout << "4.     Tampil User            " << endl;
-    cout << "5.     Kembali Menu Pengaturan" << endl;
-    cout << "Pilih Menu : ";
-    cin >> pil;
-    switch(pil)
+    do
     {
-        case 1 :
-        system("cls");
-        tambahUser(db);
-        break;
-
-        case 2:
-        system("cls");
-        deleteUser(db);
-        break;
-
-        case 3:
-        system("cls");
-        updatePassUser(db);
-        break;
-
-        case 4:
-        system("cls");
-        tampilUser(db);
-        break;
-
-        case 5:
-        system("cls");
-        break;
-
-        default:
-        cout << "Menu tidak tersedia" << endl;
-    }
-    }while(pil != 5);
-    menuPengaturan(db);
-    system("pause");
-    system("cls");
-}
-
-void menuPengaturan(sqlite3 *db)
-{
-    int pil;
-    do{
-    cout << "Menu Pengaturan" << endl;
-    cout << "1. Pengaturan Header " << endl;
-    cout << "2. Pengaturan Management User" << endl;
-    cout << "3. Kemabali Ke Menu Utama " << endl;
-    cout << "Pilih Akses : ";
-    cin >> pil;
-    switch(pil)
-    {
+        headerProgram(db, type, userSekarang);
+        cout << "Menu Management User" << endl;
+        cout << "1.     Tambah User            " << endl;
+        cout << "2.     Delete User            " << endl;
+        cout << "3.     Update Password User   " << endl;
+        cout << "4.     Tampil User            " << endl;
+        cout << "5.     Kembali Menu Pengaturan" << endl;
+        cout << "Pilih Menu : ";
+        cin >> pil;
+        switch (pil)
+        {
         case 1:
-            //header
+            system("cls");
+            headerProgram(db, type, userSekarang);
+            tambahUser(db);
             break;
 
         case 2:
             system("cls");
-            manageUser(db);
+            headerProgram(db, type, userSekarang);
+            deleteUser(db);
             break;
 
         case 3:
-            exit(0);
-        default:
-        cout << "Pilihan Menu Tidak Tersedia" << endl;
+            system("cls");
+            headerProgram(db, type, userSekarang);
+            updatePassUser(db);
+            break;
 
-    }
-    }while(pil != 3);
+        case 4:
+            system("cls");
+            headerProgram(db, type, userSekarang);
+            tampilUser(db);
+            break;
+
+        case 5:
+            break;
+
+        default:
+            cout << "Menu tidak tersedia" << endl;
+            system("cls");
+        }
+    } while (pil != 5);
+    system("cls");
 }
 
-int main()
+void manageHeader(sqlite3 *db, int &type, string &userSekarang)
 {
-    sqlite3 *db;
-    if (!checkDB(db))
+    int opsi;
+    do
     {
-        cout << "Gagal memeriksa database" << endl;
-        return 1;
-    }
-    menuPengaturan(db);
-    sqlite3_close(db);
-    return 0;
+        InformasiToko informasi = readInformasi(db);
+
+        headerProgram(db, type, userSekarang);
+        cout << "=== Informasi Toko ===" << endl;
+        cout << "Nama: " << informasi.nama << endl;
+        cout << "Alamat: " << informasi.alamat << endl;
+        cout << "Telepon: " << informasi.telp << endl;
+
+        cout << endl;
+        cout << "Opsi:" << endl;
+        cout << "1. Ubah Informasi Toko" << endl;
+        cout << "2. Keluar" << endl;
+
+        cout << "Pilih opsi: ";
+        cin >> opsi;
+
+        switch (opsi)
+        {
+        case 1:
+        {
+            system("cls");
+            headerProgram(db, type, userSekarang);
+            InformasiToko newInformasi;
+            cout << "Masukkan informasi toko baru:" << endl;
+            cout << "Nama: ";
+            cin.ignore();
+            getline(cin, newInformasi.nama);
+            cout << "Alamat: ";
+            getline(cin, newInformasi.alamat);
+            cout << "Telepon: ";
+            getline(cin, newInformasi.telp);
+
+            updateInformasi(db, newInformasi);
+            system("cls");
+            break;
+        }
+        case 2:
+            system("cls");
+            return;
+        default:
+            cout << "Opsi tidak valid. Silakan coba lagi." << endl;
+            system("cls");
+            break;
+        }
+    } while (opsi != 2);
+    return;
+}
+
+void menuPengaturan(sqlite3 *db, int &type, string &userSekarang)
+{
+    int menu;
+    bool menuState = true;
+    do
+    {
+        headerProgram(db, type, userSekarang);
+        cout << "======================================================" << endl;
+        cout << "MENU PENGATURAN" << endl;
+        cout << "======================================================" << endl;
+        cout << "1. Pengaturan Header" << endl;
+        cout << "2. Pengaturan Management User" << endl;
+        cout << "3. Kembali" << endl;
+        cout << "4. Log Out" << endl;
+        cout << "5. Keluar Program" << endl;
+        cout << "======================================================" << endl;
+        cout << "Masukkan menu: ";
+        cin >> menu;
+        FixCin();
+
+        switch (menu)
+        {
+        case 1:
+            system("CLS");
+            manageHeader(db, type, userSekarang);
+            system("PAUSE");
+            system("CLS");
+            break;
+        case 2:
+            system("CLS");
+            manageUser(db, type, userSekarang);
+            break;
+        case 3:
+            menuState = false;
+            system("CLS");
+            break;
+        case 4:
+            menuState = false;
+            system("CLS");
+            menuUser(db, type, userSekarang);
+            break;
+        case 5:
+            menuState = false;
+            system("CLS");
+            cout << "Terimakasih sudah menggunakan aplikasi ini." << endl;
+            system("PAUSE");
+            exit(0);
+        default:
+            cout << "Angka yang dimasukan salah" << endl;
+            break;
+        }
+    } while (menuState);
 }
